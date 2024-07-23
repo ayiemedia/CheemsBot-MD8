c064f5f095ac15444feddd18d8fca925adee6b06
Ayie <m4l4s14@protonmail.com>
2024-07-24 03:01:23 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['60194858428'] //ur owner number
+global.ownernomer = "60194858428" //ur owner number2
+global.ownername = "Ayiemedia" //ur owner name
+global.ytname = "YT: Ayiemedia" //ur yt chanel name
+global.socialm = "GitHub: Ayiemedia" //ur github or insta name
+global.location = "Pontian, Johor, Malaysia" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Ayiemedia"
+global.ownernumber = '60194858428'
+global.ownername = 'Ayie'
+global.ownerNumber = ["60194858428@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@ayiemedia"
+global.websitex = "https://youtube.com/@ayiemedia"
+global.wagc = "https://chat.whatsapp.com/BoEsDOz8Qtd6j852H682n5"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "Ayiemedia"
+global.botscript = 'https://github.com/ayiemedia/CheemsBot-MD8' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "Ayiemedia\n\n"
+global.creator = "60194858428@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
